0e49551e834ebec9948a2f8297007368a021973a
HeladodePistacho <th_sola@hotmail.com>
2018-06-22 18:04:33 +0200

    Added Tetris.cpp

---
+++ Engine/Tetris.cpp
@@ -0,0 +1,30 @@
+#include "ConsoleEngine.h"
+
+class TetrisGame : public ConsoleEngine
+{
+public:
+
+	TetrisGame(const wchar_t* name) : ConsoleEngine(name) {}
+
+private:
+	void GameplayStart()
+	{
+
+	}
+
+	void GameplayUpdate(float delta_time)
+	{
+
+	}
+
+	void CleanGameMemory() 
+	{
+
+	}
+};
+
+int main()
+{
+
+	return 0;
+}
